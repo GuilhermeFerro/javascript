322dbb1e2305defd32e779c822c7038fb979a263
GuilhermeFerro <guilherme.ferro1@gmail.com>
2024-04-11 09:33:22 -0300

    .

--- aulas/aula12/ex11.js
+++ aulas/aula12/ex11.js
@@ -1,8 +1,8 @@
-var idade = 22
+var idade = 18
 console.log(`Voce tem ${idade} anos.`)
-if (idade <= 16) {
+if (idade < 16) {
   console.log(`Voce nao pode votar`)
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 64) {
   console.log(`Voto opcional`)
 } else {
   console.log(`Voto obrigatorio!`)
