fbdc3c8d2608c1f0907bbf8a3ec4eb1d5d2cbca0
GuilhermeFerro <guilherme.ferro1@gmail.com>
2024-04-16 12:47:35 -0300

    .

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1) // insere um valor na variavel composta
+console.log(num.length)
+console.log(`O primeiro valor é ${num[0]}`)
+num.sort() // ordena os valores em ordem crescente
+console.log(`O primeiro valor é ${num[0]}`)
+console.log(num)
---
+++ aulas/aula15/vetoresnatela.js
@@ -0,0 +1,17 @@
+let valores = [4, 5, 8, 2, 3, 1, 7, 12]
+console.log(valores)
+/*
+for(let pos=0; pos < valores.length; pos++) {
+  console.log(`O valor da posição ${pos} é ${valores[pos]}`)
+}
+*/
+valores.sort()
+for(let pos in valores) {
+  console.log(`O valor da posição ${pos} é ${valores[pos]}`)
+}
+let pos = valores.indexOf(12)
+if (pos == -1) {
+  console.log('O valor não foi encontrado')
+} else {
+  console.log(`O valor esta na posição ${pos}`)
+}
